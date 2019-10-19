05813f913f7e5cb2ae6e1fe22bfcb29d3a68b463
ines said <inessaid@mail.usf.edu>
2019-10-19 15:55:03 -0400

    Initial commit

---
+++ Assets/add_scene_on_click.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class add_scene_on_click : MonoBehaviour
+{
+    public void LoadByIndex(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
